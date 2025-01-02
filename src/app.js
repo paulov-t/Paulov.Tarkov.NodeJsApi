fe20d02a83fd4af3db470223a63b99559cb44df9
Paulov-t <pargsoft@hotmail.com>
2025-01-02 00:56:26 +0000

    add back ejs package

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use(express.raw({ type: "application/json", limit: '50mb',
   parameterLimit: 100000,
   extended: true  }));
