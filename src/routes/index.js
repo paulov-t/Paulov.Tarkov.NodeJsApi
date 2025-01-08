8a6874aa6245a9c57abc99c01389ea4af6b0c8d0
Paulov-t <fmt-dev@outlook.com>
2025-01-08 12:20:46 +0000

    Add Server Mode Environment variable

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,12 @@ var bsgHelper =  require('./../bsgHelper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Paulov TWS' });
+
+  console.log(process.env);
+
+  const serverMode = process.env && process.env.ServerMode ? process.env.ServerMode : "Dev";
+
+  res.render('index', { title: 'Paulov-t Tarkov Web Server', serverMode: serverMode });
 });
 
 module.exports = router;
