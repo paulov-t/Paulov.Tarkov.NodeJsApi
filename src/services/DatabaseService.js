a68189be529c66aa203156280b25e549ff349cec
Paulov-t <fmt-dev@outlook.com>
2025-01-18 23:43:04 +0000

    add getTemplateQuestsAsList function

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -5,6 +5,17 @@ class DatabaseService {
     getDatabase() {
         return Database;
     }
+
+    getTemplateQuestsAsList() {
+        const templateQuests = this.getDatabase().getTemplateQuests();
+        const quests = [];
+        
+        for(const questId in templateQuests) {
+            quests.push(templateQuests[questId]);
+        }
+
+        return quests;
+    }
 }
 
 module.exports.DatabaseService = new DatabaseService();
