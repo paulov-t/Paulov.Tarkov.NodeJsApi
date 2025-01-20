cb151b4ffcc8751a50a987870c147476577dbcf0
Paulov-t <fmt-dev@outlook.com>
2025-01-20 22:44:46 +0000

    add sendRagfairOfferSold and sendRagfairNewRating

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -1,3 +1,6 @@
+const { ENotificationType } = require('../models/ENotificationType');
+const { generateMongoId } = require('./../bsgHelper');
+
 /**
  * A service to retain connections for each user logged in to the App
  */
@@ -8,6 +11,50 @@ class WebSocketService {
          */
         this.connections = {};
     }
+
+
+    /**
+     * 
+     * @param {*} accountId 
+     * @param {*} offerId 
+     * @param {*} handbookId 
+     * @param {*} count 
+     */
+    sendRagfairOfferSold(accountId, offerId, handbookId, count) {
+
+        if(WebSocketService.connections[accountId])
+            WebSocketService.connections[accountId].socket.send(JSON.stringify(
+            { 
+                type: ENotificationType.RagfairOfferSold
+                , eventId: generateMongoId()
+                , offerId: offerId
+                , handbookId: handbookId
+                , count: count
+                , time: 5 
+            }));
+    }
+
+    /**
+     * 
+     * @param {*} accountId 
+     * @param {*} rating 
+     * @param {*} isRatingGrowing 
+     */
+    sendRagfairNewRating(accountId, rating, isRatingGrowing) {
+
+        if(WebSocketService.connections[accountId])
+            WebSocketService.connections[accountId].socket.send(JSON.stringify(
+            { 
+                type: ENotificationType.RagfairNewRating
+                , eventId: generateMongoId()
+                , rating: rating
+                , isRatingGrowing: isRatingGrowing
+                , time: 5 
+            }));
+    }
 }
+
+
+
 
 module.exports.WebSocketService = new WebSocketService();
