d08e50c9fcb2945f9ea307c605a10f6a7835dd5f
Paulov-t <fmt-dev@outlook.com>
2025-01-23 11:06:01 +0000

    Add PlayerVisualRepresentation model and Service

--- src/models/PlayerVisualRepresentation.js
+++ src/models/PlayerVisualRepresentation.js
@@ -2,8 +2,25 @@ class PlayerVisualRepresentation {
     constructor() {
         this.Info = {};
         this.Customization = {};
-        this.Equipment = {};
+        /**
+         * @type {PlayerVisualRepresentationEquipment}
+         */
+        this.Equipment = new PlayerVisualRepresentationEquipment();
+    }
+}
+
+class PlayerVisualRepresentationEquipment {
+    constructor() {
+        /**
+         * @type {String}
+         */
+        this.Id = "";
+        /**
+         * @type {Array}
+         */
+        this.Items = [];
     }
 }
 
 module.exports.PlayerVisualRepresentation = PlayerVisualRepresentation;
+module.exports.PlayerVisualRepresentationEquipment = PlayerVisualRepresentationEquipment;
---
+++ src/services/PlayerVisualRepresentationService.js
@@ -0,0 +1,42 @@
+const { AccountProfileCharacter } = require('../models/Account');
+const { Inventory } = require('../models/Inventory');
+const { PlayerVisualRepresentation, PlayerVisualRepresentationEquipment } = require('./../models/PlayerVisualRepresentation');
+
+/**
+ * A service to create a PlayerVisualRepresentation instance object
+ */
+class PlayerVisualRepresentationService {
+    constructor() {
+
+    }
+
+    /**
+     * Create's a {PlayerVisualRepresentation} from a {AccountProfileCharacter} instance
+     * @param {AccountProfileCharacter} character 
+     * @returns {PlayerVisualRepresentation} 
+     */
+    createPlayerVisualRepresentation(character) {
+        const pvr = new PlayerVisualRepresentation();
+        pvr.Customization = character.Customization;
+        pvr.Info = character.Info;
+        pvr.Equipment = this.createPlayerVisualRepresentationEquipmentFromInventory(character.Inventory);
+        return pvr;
+    }
+
+    /**
+     * Create's a {PlayerVisualRepresentationEquipment} from an {Inventory} instance
+     * @param {Inventory} inventory 
+     * @returns {PlayerVisualRepresentationEquipment}
+     */
+    createPlayerVisualRepresentationEquipmentFromInventory(inventory) {
+        const pvre = new PlayerVisualRepresentationEquipment();
+
+        pvre.Id = inventory.equipment;
+        pvre.Items = inventory.items;
+
+        return pvre;
+
+    }
+}
+
+module.exports.PlayerVisualRepresentationService = PlayerVisualRepresentationService;
